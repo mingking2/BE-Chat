e7cd70c750dfe42e0d443df57d15a91a3b487524
King's meow <mingee641@gmail.com>
2023-08-21 16:43:25 +0900

    [ADD] UserRepository: db초기화를 위한 id초기화 기능

--- src/main/java/com/toyproject/authsystem/repository/UserRepository.java
+++ src/main/java/com/toyproject/authsystem/repository/UserRepository.java
@@ -2,9 +2,15 @@ package com.toyproject.authsystem.repository;
 
 import com.toyproject.authsystem.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Modifying
+    @Query(value = "ALTER TABLE user AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetIdSequence();
 }
