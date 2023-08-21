ab0f456b6c8de82f3237fe0aa1583bd7445b5062
King's meow <mingee641@gmail.com>
2023-08-21 16:44:47 +0900

    [ADD] UserService: 서비스단위에서 ResponseEntity 사용X, 로직 단순화? SpringSecurity 제거, db 초기화 기능 추가

--- src/main/java/com/toyproject/authsystem/service/UserService.java
+++ src/main/java/com/toyproject/authsystem/service/UserService.java
@@ -2,11 +2,10 @@ package com.toyproject.authsystem.service;
 
 import com.toyproject.authsystem.domain.entity.User;
 import com.toyproject.authsystem.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,31 +14,42 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
     private final UserRepository userRepository;
 
-    private final PasswordEncoder passwordEncoder;
 
-    public ResponseEntity<User> register(User newUser) {
+//    public ResponseEntity<User> register(User newUser) {
+//        // 이메일 중복 확인
+//        User existingUser = userRepository.findByEmail(newUser.getEmail());
+//        if(existingUser != null) {
+//            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+//        }
+//
+//        newUser = userRepository.save(newUser);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+//    }
+    public User register(User newUser) {
         // 이메일 중복 확인
         User existingUser = userRepository.findByEmail(newUser.getEmail());
         if(existingUser != null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            return null;
         }
 
-        // 비밀번호 암호화
-        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         newUser = userRepository.save(newUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        return newUser;
     }
 
-    public ResponseEntity<String> login(String email, String password) {
+    public User login(String email, String password) {
         User user = userRepository.findByEmail(email);
 
-        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        } else {
+            return null;
         }
+    }
 
-        return ResponseEntity.ok("로그인 성공!");
+    public void resetData() {
+        userRepository.deleteAll();
+        userRepository.resetIdSequence();
     }
 }
