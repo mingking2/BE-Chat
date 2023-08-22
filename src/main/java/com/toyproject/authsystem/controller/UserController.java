b6bb4ffeacf6b1ec22fb20852acabf45030f25d4
King's meow <mingee641@gmail.com>
2023-08-22 16:51:36 +0900

    [ADD] 이해를 위한 주석 추가

--- src/main/java/com/toyproject/authsystem/controller/UserController.java
+++ src/main/java/com/toyproject/authsystem/controller/UserController.java
@@ -5,7 +5,6 @@ import com.toyproject.authsystem.domain.entity.User;
 import com.toyproject.authsystem.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +16,29 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * 로그인 후 홈화면이라 가정
+     * @param user // @SessionAttribute를 사용하여 이름이 "user"인 객체를 value 값으로 가진 sessionId가 있으면 객체를 반환
+     * @param session // sessionId 값을 보여주기 위함
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<?> home(@SessionAttribute(name = "user", required = false) User user, HttpSession session) {
+        if(user == null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("로그인 하지 않음");
+        }
+        else {
+            String sessionId = session.getId();
+            return ResponseEntity.ok().body(String.format("로그인 유저의 이메일: %s, 세션 ID: %s", user.getEmail(), sessionId));
+        }
+    }
+
+
+    /**
+     * 회원가입하는 로직
+     * @param user // 회원가입을 위한 user 객체 불러옴
+     * @return registeredUser // ResponseEntity로 상태를 반환하고 body부분에 회원가입한 계정의 Json타입으로 출력
+     */
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
         User registeredUser = userService.register(user);
@@ -28,18 +50,21 @@ public class UserController {
         }
     }
 
+    /**
+     * 로그인을 위한 로직
+     * @param email // 쿼리스트링으로 아이디에 해당하는 email값
+     * @param password // 비밀번호
+     * @param session // 해당 파리미터를 퉁해 선언이 되면서 sessionId를 생성하고
+     *                      session.setAttribute를 이용하여 해당 아이디에 value값으로 user 객체를 넣어준다.
+     * @return
+     */
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password, HttpSession session) {
-        // 로그인 상태 확인
-        User loggedinUser = (User) session.getAttribute("user");
-        if(loggedinUser != null) {
-             return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 로그인된 상태이다.");
-        }
 
         User user = userService.login(email, password);
         if (user != null) {
-            session.setAttribute("user", user);
             // 로그인 성공 시, 사용자의 email 및 세션 ID 출력
+            session.setAttribute("user", user);
             return ResponseEntity.ok().body(
                     String.format("로그인 성공! 이메일: %s, 세션 ID: %s", user.getEmail(), session.getId()));
         } else {
@@ -47,6 +72,11 @@ public class UserController {
         }
     }
 
+    /**
+     * 로그아웃을 위한 로직
+     * @param session // 로그아웃을 위해 기존 세션id를 불러온다.
+     * @return
+     */
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpSession session) {
         User loginUser = (User) session.getAttribute("user");
--- src/main/java/com/toyproject/authsystem/service/UserService.java
+++ src/main/java/com/toyproject/authsystem/service/UserService.java
@@ -13,6 +13,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    // newUser 객체에 있는 이메일의 중복 확인 후 db에 저장하고 해당 객체 반환
     public User register(User newUser) {
         // 이메일 중복 확인
         User existingUser = userRepository.findByEmail(newUser.getEmail());
@@ -24,6 +25,7 @@ public class UserService {
         return newUser;
     }
 
+    // 이메일과 비밀번호가 db와 일치하는지 확인해서 일치하면 객체 반환
     public User login(String email, String password) {
         User user = userRepository.findByEmail(email);
 
@@ -34,6 +36,7 @@ public class UserService {
         }
     }
 
+    // test를 위한 db초기화기능 넣음
     public void resetData() {
         userRepository.deleteAll();
         userRepository.resetIdSequence();
