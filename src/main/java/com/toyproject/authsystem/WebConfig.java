f15c327d88928ffcd2706d8a479375354f1b64b0
King's meow <mingee641@gmail.com>
2023-08-28 17:56:26 +0900

    [ADD] REACT랑 연동을 위한 파일 추가

---
+++ src/main/java/com/toyproject/authsystem/WebConfig.java
@@ -0,0 +1,29 @@
+package com.toyproject.authsystem;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080","http://localhost:3000") // 허용할 원격 주소
+                .allowedMethods("*") // 허용할 HTTP 메서드 (GET, POST, PUT, DELETE 등)
+                .allowedHeaders("*") // 허용할 HTTP 헤더
+                .allowCredentials(true) // 쿠키 전송 허용
+
+                .maxAge(3600); // 사전 전달 요청(Preflight request)의 캐시 시간
+    }
+
+}
